c4591619ebcadf27ffcda1b4445e2a7041701a4f
asprgs <tayu.udosu@gmail.com>
2025-07-11 18:26:20 +0900

    GameLogic.java

--- game/GameLogic.java
+++ game/GameLogic.java
@@ -1,9 +1,12 @@
+//GameLogic.java
 //ヒント作成など
 
 package game;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GameLogic {
 
@@ -19,18 +22,19 @@ public class GameLogic {
     }
 
     //正解に含まれる文字を返す
-    public static List<Character> findIncludedLetters(String answer, String guess) {
-        List<Character> list = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            char ans = answer.charAt(i);
-            for (int j = 0; j < 5; j++){
-                char gue = guess.charAt(j);
-                if (ans == gue) {
-                    list.add(gue);
-                }
-            }
-        }
-        return list;
+public static List<Character> findIncludedLetters(String answer, String guess) {
+    Map<Character, Integer> answerFreq = new HashMap<>();
+    for (char c : answer.toCharArray()) {
+        answerFreq.put(c, answerFreq.getOrDefault(c, 0) + 1);
     }
 
+    List<Character> result = new ArrayList<>();
+    for (char c : guess.toCharArray()) {
+        if (answerFreq.getOrDefault(c, 0) > 0) {
+            result.add(c);
+            answerFreq.put(c, answerFreq.get(c) - 1); // カウントを減らす
+        }
+    }
+    return result;
+}
 }
